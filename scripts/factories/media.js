feed8b26109b2baa931fbf125ce3b0313ec6c066
LouisMichelin <louis.michelin097@gmail.com>
2023-04-23 18:20:52 +0200

    Rename correctement 2x images chez Marcel

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,40 +1,39 @@
 function mediaFactory(data) {
     // Ici, c'est les sous-parties de la sous-partie "MEDIA" du .JSON que l'on veut filtrer :
     const { id, photographerId, name, title, image, video, likes, date, price } = data;
-    // pour en retourner data.id, data.photographerId, ...
+    // pour retourner : data.id, data.photographerId, data.name, etc
   
+    // Sépare le Prénom & le Nom avec .split() + Prend l'index [0] pour le prénom + Remplace "-" par " ".
     let prenom = name.split(" ")[0].replace("-", " ");
 
-    // Chemin vers les photos & vidéos
-    // const imageSource = `/FishEye - Photos/Sample Photos/${filtreTest42}/${image}` // TENTATIVE DE FILTRE-PRENOM par ID
+    // Source dynamique vers les medias :
     const imageSource = `/FishEye - Photos/Sample Photos/${prenom}/${image}`
-    const videoSource = `/FishEye - Photos/Sample Photos/${prenom}/${video}` // CE SONT LES MEDIAS DE ELLIE ROSE EN LOCAL
+    const videoSource = `/FishEye - Photos/Sample Photos/${prenom}/${video}`
     
-   
-    
-    // Création des medias du photographe choisi
+    // Création des medias du photographer choisi :
     function getMediaCardDOM() {
         // Balise <a>
         const link = document.createElement('a');
-        link.setAttribute('href', `${imageSource}`);
+        link.setAttribute('href', imageSource);
         // Titre des medias
         const h2 = document.createElement('h2');
         h2.textContent = title;
-        // Condition SI image OU SI video
+        // Condition SI image / SI video
         if (image) {
             const testImage = document.createElement('img');
             testImage.setAttribute('src', imageSource);
-            testImage.setAttribute('height', '100px');
-            testImage.setAttribute('width', '100px');
+            testImage.setAttribute('height', '150px');
+            testImage.setAttribute('width', '150px');
             link.appendChild(h2);
             h2.appendChild(testImage);
-        } else if (video) {
+        };
+        if (video) {
             const testVideo = document.createElement('video');
             testVideo.setAttribute('src', videoSource);
             testVideo.setAttribute('width', '150px');
             link.appendChild(h2);
             h2.appendChild(testVideo);
-        }
+        };
         return link;
     };
     return { id, photographerId, name, title, image, video, likes, date, price, getMediaCardDOM };
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,22 +2,20 @@
 const params = new URLSearchParams(window.location.search);
 const selectedArtist = params.get('id');
 
+// Définition globale "photographer" :
 let photographer;
-// console.log(selectedArtist);
 
-// Photographes sélectionnés :
+// Photographes sélectionnés depuis le .JSON :
 async function getPhotographers() {
     const response = await fetch('/data/photographers.json');
     const photographers = await response.json();
     return photographers;
 };
 
-// Filtre le photographe avec l'ID récupéré
+// Filtre le photographe avec l'ID récupéré :
 function filterPhotographerById(photographers) {
-    photographer = photographers.filter((element) =>
-        (element.id) == selectedArtist    
-    )[0];
-
+    // 
+    photographer = photographers.filter((element) => (element.id) == selectedArtist)[0];
     // On envoie les données filtrées au Factory
     const photographerModel = photographerFactory(photographer);
     // Ces données passent ensuite vers la fonction getUserHeaderDOM()
@@ -29,7 +27,7 @@ function filterPhotographerById(photographers) {
 
 
 
-// Filtre les médias grâce au photographerId et l'ID récupéré
+// Filtre les médias grâce au photographerId et l'ID récupéré :
 function filterMediasById(medias) {
     const photographerMedia = medias.filter((element) =>
         (element.photographerId) == selectedArtist
@@ -37,19 +35,19 @@ function filterMediasById(medias) {
     // Sélection de la balise <main> dans le HTML
     const main = document.getElementById('main');
     
-    // Exécution de la fonction mediaFactory() pour récupérer les données ligne3 factory
-    // et ensuite exécution de getMediaCardDOM() .forEach(element) dont on a besoin.
+    // Exécute mediaFactory() pour récupérer données ligne 3 de media.js
+    // et exécute getMediaCardDOM() dans le .forEach(element) pour chaque média.
     photographerMedia.forEach((element) => {
-        
-        
+        // Définit mediaData pour récupérer TOUT "element" en forme .JSON
         const mediaData = {...element,
-            "name": photographer.name};
-
+            "name": photographer.name
+        };
+            // Maintenant, on a injecté la variable "name" avec dans "element"
             console.log(mediaData);
             const mediaModel = mediaFactory(mediaData);
             const mediaCardDOM = mediaModel.getMediaCardDOM();
             main.appendChild(mediaCardDOM);
-        });
+    });
 };
 
 async function init() {
